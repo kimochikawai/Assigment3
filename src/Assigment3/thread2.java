5b9bba155e1a85bc79c4a7aab017c1569f4acd67
kimochikawai <truongnxth1805028@fpt.edu.vn>
2019-04-11 22:00:44 +0700

    first commit

---
+++ src/Assigment3/thread2.java
@@ -0,0 +1,13 @@
+package Assigment3;
+
+public class thread2 extends Thread {
+    public void run(){
+        try {
+
+
+            for (int i=1;i<=50;i++){
+                System.out.println(i);
+                Thread.sleep(500);
+            }}catch (Exception e){}
+    }
+}
